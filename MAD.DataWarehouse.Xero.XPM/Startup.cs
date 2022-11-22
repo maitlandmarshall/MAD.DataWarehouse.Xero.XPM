8aae95b0509777c85cde5ac333538651c3728357
maitlandmarshall <maitlandmarshallapps@gmail.com>
2022-11-22 16:34:21 +1000

    Storage encryption is required

--- MAD.DataWarehouse.Xero.XPM/Startup.cs
+++ MAD.DataWarehouse.Xero.XPM/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.DependencyInjection;
 using MIFCore.Common;
 using MIFCore.Settings;
 using OAuthB0ner;
+using OAuthB0ner.Storage;
 using OAuthB0ner.Storage.SqlServer;
 
 namespace MAD.DataWarehouse.Xero.XPM
@@ -27,6 +28,14 @@ namespace MAD.DataWarehouse.Xero.XPM
             );
 
             serviceDescriptors.AddOAuthB0nerSqlServerStorage(opt => this.configuration.Bind(opt));
+        }
+
+        public void Configure(StorageOptions storageOptions)
+        {
+            // This is a requirement from Xero itself.
+            // https://www.absia.asn.au/industry-standards/addon-security-standard/ABSIA-Security-Standard-for-Add-on-Marketplaces.pdf
+            if (storageOptions.IsEncryptionOn == false)
+                throw new XeroSecurityRequirementException("Encryption is required for Xero token storage. Please set the 'oauth:storage:isEncryptionOn' configuration value to 'true'.");
         }
 
         public void PostConfigure()
---
+++ MAD.DataWarehouse.Xero.XPM/XeroSecurityRequirementException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace MAD.DataWarehouse.Xero.XPM
+{
+    [Serializable]
+    internal class XeroSecurityRequirementException : Exception
+    {
+        public XeroSecurityRequirementException()
+        {
+        }
+
+        public XeroSecurityRequirementException(string message) : base(message)
+        {
+        }
+
+        public XeroSecurityRequirementException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected XeroSecurityRequirementException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
